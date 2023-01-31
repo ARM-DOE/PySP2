b859570b22088f0d51b7b198cc92b2378a02cafe
rcjackson <rcjackson@users.noreply.github.com>
2023-01-31 17:49:43 +0000

    deploy: 2ee8a29602cd16ad0c5b0fe3c20fe5acb490f52d

--- _downloads/5247039f1f38d2c006d2f3ebecb4fce3/plot_read_sp2b.py
+++ _downloads/5247039f1f38d2c006d2f3ebecb4fce3/plot_read_sp2b.py
@@ -9,5 +9,6 @@ import matplotlib.pyplot as plt
 my_sp2 = pysp2.io.read_sp2(pysp2.testing.EXAMPLE_SP2B)
 my_config = pysp2.io.read_config(pysp2.testing.EXAMPLE_INI)
 my_sp2 = pysp2.util.gaussian_fit(my_sp2, my_config)
+print(my_sp2)
 pysp2.vis.plot_wave(my_sp2, 1100, 0)
 plt.show()
