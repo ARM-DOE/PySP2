17c1a5e489aba70ff4320ea32f1bd3fa03bd358a
Robert Jackson <rjackson@evswl136.evs.anl.gov>
2023-03-30 13:32:53 -0500

    STY: Change to pep8 and version 1.3.0

--- pysp2/vis/plot_wave.py
+++ pysp2/vis/plot_wave.py
@@ -5,7 +5,8 @@ import xarray as xr
 from ..util import _gaus
 
 
-def plot_wave(ds, record_no, chn, plot_fit=True, append_to_display=False,init_kwargs=None, **kwargs):
+def plot_wave(ds, record_no, chn, plot_fit=True,
+              append_to_display=False, init_kwargs=None, **kwargs):
     """
     Plots the raw waveform for a given record_no and channel.
 
@@ -21,7 +22,7 @@ def plot_wave(ds, record_no, chn, plot_fit=True, append_to_display=False,init_kw
         Set to True to plot the gaussian fit. Only used for channels 0 and 4.
     append_to_display : False or ACT HistogramDisplay object
         Default is False. Give an existing ACT HistogramDisplay object to append
-        to instead of creating a new one. init_kwargs does not work for an 
+        to instead of creating a new one. init_kwargs does not work for an
         existing display object.
     init_kwargs: dict
         Dictionary of keyword arguments to pass into initialization of
@@ -46,8 +47,8 @@ def plot_wave(ds, record_no, chn, plot_fit=True, append_to_display=False,init_kw
     inp_data = xr.Dataset(inp_data)
     bins = np.linspace(0, 100, 100)
     if init_kwargs is None:
-        if append_to_display != False:
-            display=append_to_display
+        if append_to_display is not False:
+            display = append_to_display
         else:
             display = act.plotting.HistogramDisplay(inp_data)
     else:
@@ -72,14 +73,19 @@ def plot_wave(ds, record_no, chn, plot_fit=True, append_to_display=False,init_kw
         width = spectra['PkFWHM_ch' + str(chn)].values
         Y = _gaus(xspace, amplitude, pos, width/2., base)
         ax.plot(xspace, Y)
-        ax.text(0.7, 0.5, 'Fit Pos = %3.2f' % pos, transform=ax.transAxes)
-        ax.text(0.7, 0.6, 'Fit Half Width = %3.2f' % width, transform=ax.transAxes)
-        ax.text(0.7, 0.7, 'Fit Baseline = %3.2f' % base, transform=ax.transAxes)
-        ax.text(0.7, 0.8, 'Fit Amplitude = %3.2f' % amplitude, transform=ax.transAxes)
+        ax.text(0.7, 0.5, 'Fit Pos = %3.2f' % pos,
+                transform=ax.transAxes)
+        ax.text(0.7, 0.6, 'Fit Half Width = %3.2f' % width,
+                transform=ax.transAxes)
+        ax.text(0.7, 0.7, 'Fit Baseline = %3.2f' % base,
+                transform=ax.transAxes)
+        ax.text(0.7, 0.8, 'Fit Amplitude = %3.2f' % amplitude,
+                transform=ax.transAxes)
 
     return display
 
-def plot_waves(ds,record_no,plot_fit=True):
+
+def plot_waves(ds, record_no, plot_fit=True):
     """
     Plots the raw waveforms of all channels for a given record_no.
 
@@ -97,30 +103,36 @@ def plot_waves(ds,record_no,plot_fit=True):
         Returns the ACT
     """
 
-    chns=[i for i in range(8)]
-    panel_number=[0,1,1,2,0,1,1,2]
-    legends=[['SCHG ch0','ch0_fit','SCLG ch4','ch4_fit'],['BBHG ch1','NBHG ch2','BBLG ch5','NBLG ch6'],['ch3 HG','CH7 LG']]
- 
-    if ds['ScatRejectKey'].isel(event_index=record_no)==3:
-        display=plot_wave(ds,record_no,0,plot_fit=False,
-                                init_kwargs={'subplot_shape':(3,)})
+    chns = [i for i in range(8)]
+    panel_number = [0, 1, 1, 2, 0, 1, 1, 2]
+    legends = [['SCHG ch0', 'ch0_fit', 'SCLG ch4', 'ch4_fit'],
+               ['BBHG ch1', 'NBHG ch2', 'BBLG ch5', 'NBLG ch6'],
+               ['ch3 HG', 'CH7 LG']]
+
+    if ds['ScatRejectKey'].isel(event_index=record_no) == 3:
+        display = plot_wave(ds, record_no, 0, plot_fit=False,
+                            init_kwargs={'subplot_shape': (3, )})
         legends[0].remove('ch0_fit')
     else:
-        display=plot_wave(ds,record_no,0,plot_fit=plot_fit,
-                                    init_kwargs={'subplot_shape':(3,)})
+        display = plot_wave(ds, record_no, 0, plot_fit=plot_fit,
+                            init_kwargs={'subplot_shape': (3, )})
         if plot_fit:
             for t in display.axes[0].texts:
-                pos=t.get_position()
-                t.set_position((0.1,pos[1]))
-    for i,panel in enumerate(panel_number):
-        if i!=0:
-            display=plot_wave(ds,record_no,chns[i],plot_fit=plot_fit,
-                                    append_to_display=display,
-                                    subplot_index=(panel,))
-    titles=['Scattering chanels','Incandesence channels','Split detector channels']
-    xlabels=['','','Bin #']
-    ylabels=['Data_ch0, Data_ch4','Data_ch1, Data_ch2\nData_ch5, Data_ch6','Data_ch3, Data_ch7']
-    for i,ax in enumerate(display.axes):
+                pos = t.get_position()
+                t.set_position((0.1, pos[1]))
+    for i, panel in enumerate(panel_number):
+        if i != 0:
+            display = plot_wave(ds, record_no, chns[i], plot_fit=plot_fit,
+                                append_to_display=display,
+                                subplot_index=(panel,))
+    titles = ['Scattering chanels',
+              ' Incandesence channels',
+              'Split detector channels']
+    xlabels = ['',  '', 'Bin #']
+    ylabels = ['Data_ch0, Data_ch4',
+               'Data_ch1, Data_ch2\nData_ch5, Data_ch6',
+               'Data_ch3, Data_ch7']
+    for i, ax in enumerate(display.axes):
         ax.set_title(titles[i])
         ax.set_xlabel(xlabels[i])
         ax.set_ylabel(ylabels[i])
