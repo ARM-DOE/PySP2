f95e218245d724a78510b6785be0a2eac3f81dfd
John Backman <john.backman@fmi.fi>
2024-04-03 17:55:57 +0300

    typo

--- tests/test_gfit.py
+++ tests/test_gfit.py
@@ -11,7 +11,7 @@ def test_gaussian_fit():
         np.nanmax(my_binary.PkHt_ch0.values), 98708.92915295, decimal=1)
     np.testing.assert_almost_equal(
         np.nanmax(my_binary.PkHt_ch4.values), 65088.3959945008, decimal=1)
-    # check that there are requal amounts of successful fits for low gain and
+    # check that there are equal amounts of successful fits for low gain and
     # high gain scattering when the peak heighs are large enough
     bl_hg = np.logical_and(my_binary['FtAmp_ch0'] > 30000,
                            my_binary['FtAmp_ch0'] < 50000)
